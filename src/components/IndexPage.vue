bf4f2330ac79a272e57984c24bffc169fdaed891
Rashidik Sadykov <rashidiksadykov@gmail.com>
2024-12-13 18:52:12 +0500

    Stats

---
+++ src/components/IndexPage.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const numOfCores = navigator.hardwareConcurrency;
+
+
+
+</script>
+
+<template>
+  <div class="card">
+<p>
+Cores {{numOfCores}}
+</p>
+<p>
+Altyn: {{numOfCores}}
+</p>
+
+    <button type="button">Start</button>
+    
+  </div>
+
+  
+</template>
+
+<style scoped>
+</style>
